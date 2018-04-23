1ca62177bfb9706ac03ad0534e6490d74cfe168e
Spencer Negri <SpencerNegri@engr2-30-50-dhcp.int.colorado.edu>
2018-04-23 17:34:05 -0600

    Changes

--- calculator.py
+++ calculator.py
@@ -1,6 +1,26 @@
-csv = open("Save2.0Data.csv", "r")
+def calc():
+	csv = open("Save2.0Data.csv", "r")
+	for line in csv.readlines():
+		line = line.split(',')
+		if line[5] == prevName:
+			continue
+		prevName = line[5]
+		scoreDiff = line[0]
+		inningCount = line[1]
+		outsCount = line[6]
+		baserunnerCount = 0
+		if line[2] != "":
+			 baserunnerCount += 1
+		if line[3] != "":
+                 baserunnerCount += 1
+		if line[4] != "":
+			 baserunnerCount += 1
+		print "Score Diff:" + str(scoreDiff) + ", Inning Count:" + str(inningCount) + ", Outs Count" + str(outsCount)
 
+def main():
+	prevname = ""
+	calc()
+	
 
-for line in csv.readlines():
-	line = line.split(',')
-	print line
+if __name__== "__main__":
+	main()
