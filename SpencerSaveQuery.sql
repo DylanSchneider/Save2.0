138ace1b42c9f03ae1f87d34c8afe9b983915699
Dylan Schneider <DylanDLS@gmail.com>
2018-04-23 16:52:17 -0600

    changed spencers query a little bit

--- SpencerSaveQuery.sql
+++ SpencerSaveQuery.sql
@@ -1,5 +1,6 @@
+use retrosheet;
     
-SELECT ABS(g.AWAY_SCORE_CT - g.HOME_SCORE_CT) AS ScoreDiff, g.INN_CT AS Inning, e.BASE1_RUN_ID, e.BASE2_RUN_ID, e.BASE3_RUN_ID, e.pit_id
+SELECT e.game_ID, ABS(g.AWAY_SCORE_CT - g.HOME_SCORE_CT) AS ScoreDiff, e.INN_CT AS Inning, e.OUTS_CT as 'Current Outs', e.BASE1_RUN_ID, e.BASE2_RUN_ID, e.BASE3_RUN_ID, e.pit_id
 FROM games g
 JOIN events e ON e.pit_id = g.save_pit_id AND e.game_ID = g.game_ID;
 
